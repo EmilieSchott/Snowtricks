df722ebf7eb652a5571acd5cf7b9b75be5d5ed3d
Emilie Schott <emilie.schott@gmail.com>
2021-05-19 17:34:26 +0200

    Create DeleteFigureController

---
+++ src/Controller/DeleteFigureController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DeleteFigureController extends AbstractController
+{
+    /**
+     * @Route("/delete/figure", name="delete_figure")
+     */
+    public function index(): Response
+    {
+        return new Response(
+            <<<'EOF'
+        <h1>Suppression d'une figure</h1>
+EOF
+        );
+        /*return $this->render('delete_figure/index.html.twig', [
+            'controller_name' => 'DeleteFigureController',
+        ]);*/
+    }
+}
