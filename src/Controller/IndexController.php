5b05001c3d661847c8687962f4f7eec81359e512
Emilie Schott <emilie.schott@gmail.com>
2021-05-19 17:13:15 +0200

    Create IndexController and template related

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return new Response(
+            <<<'EOF'
+        <h1>Homepage</h1>
+EOF
+        );
+        /*return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);*/
+    }
+}
