360ffd0d2a868e454fe46b15562c19e004458f21
Emilie Schott <emilie.schott@gmail.com>
2021-06-03 10:07:42 +0200

    Controller modifications to use twig and comment repository

--- src/Controller/ShowFigureController.php
+++ src/Controller/ShowFigureController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,12 @@ class ShowFigureController extends AbstractController
     /**
      * @Route("/figure/{slug}", name="show_figure")
      */
-    public function show(): Response
+    public function show(Figure $figure, CommentRepository $commentRepository): Response
     {
         return $this->render('show_figure.html.twig', [
             'controller_name' => 'ShowFigureController',
+            'figure' => $figure,
+            'comments' => $commentRepository->findBy(['figure' => $figure], ['createdAt' => 'DESC']),
         ]);
     }
 }
