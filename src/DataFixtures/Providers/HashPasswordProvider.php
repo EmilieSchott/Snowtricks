efd104c6023a5e17e5350f695921227a6a71fe15
Emilie Schott <emilie.schott@gmail.com>
2021-06-03 14:23:15 +0200

    bug-fix during fixtures generation :  Unknown formatter 'hashPassword'

---
+++ src/DataFixtures/Providers/HashPasswordProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\DataFixtures\Provider;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class HashPasswordProvider
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function hashPassword(string $plainPassword): string
+    {
+        return $this->encoder->encodePassword(new User(), $plainPassword);
+    }
+}
