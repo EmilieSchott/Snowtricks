b214e300d6e9b0da39566a35ecec85db2f2db6d0
Emilie Schott <emilie.schott@gmail.com>
2021-06-10 10:18:42 +0200

    Modify Controller to display form in create and modify methods

--- src/Controller/FigureFormController.php
+++ src/Controller/FigureFormController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Figure;
+use App\Form\FigureFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,18 +15,25 @@ class FigureFormController extends AbstractController
      */
     public function createFigure(): Response
     {
+        $figure = new Figure();
+        $form = $this->createForm(FigureFormType::class, $figure);
+
         return $this->render('figure_form.html.twig', [
             'controller_name' => 'FigureFormController',
+            'figure_form' => $form->createView(),
         ]);
     }
 
     /**
      * @Route("/figure/form/{slug}", name="modify_figure")
      */
-    public function modifyFigure(): Response
+    public function modifyFigure(Figure $figure): Response
     {
+        $form = $this->createForm(FigureFormType::class, $figure);
+
         return $this->render('figure_form.html.twig', [
             'controller_name' => 'FigureFormController',
+            'figure_form' => $form->createView(),
         ]);
     }
 }
