5c671b2b32ed2c4d31c138b6e38d949778c0581a
Emilie Schott <emilie.schott@gmail.com>
2021-06-10 18:27:20 +0200

    Controller modifications to persist datas comment in database

--- src/Controller/ShowFigureController.php
+++ src/Controller/ShowFigureController.php
@@ -6,9 +6,9 @@ use App\Entity\Comment;
 use App\Entity\Figure;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
-use App\Repository\ImageRepository;
-use App\Repository\VideoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +17,18 @@ class ShowFigureController extends AbstractController
     /**
      * @Route("/figure/{slug}", name="show_figure")
      */
-    public function show(Figure $figure, ImageRepository $imageRepository, VideoRepository $videoRepository, CommentRepository $commentRepository): Response
+    public function show(Request $request, Figure $figure, CommentRepository $commentRepository, EntityManagerInterface $entityManager): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setFigure($figure);
+            $comment->setUser($this->getUser());
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+        }
 
         return $this->render('show_figure.html.twig', [
                     'controller_name' => 'ShowFigureController',
