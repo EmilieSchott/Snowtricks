132364f17c6aa2be446d37beb3f913f37c5f10ba
Emilie Schott <emilie.schott@gmail.com>
2021-06-28 18:15:20 +0200

    feature/homepage|REBASE 34/34

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Comment;
+use App\Entity\Figure;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
